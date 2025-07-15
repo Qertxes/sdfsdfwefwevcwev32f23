cc1b4e1c2e98ef09e96da1f353eb4e2b355fa073
Qertxes <qertxes@mail.ru>
2025-07-15 22:32:23 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,49 @@
+TOKEN = "8138338715:AAGAgSE17773ciPvk9hcNKb3btYzdfBgmlo" #сюда токен бота
+
+admins_id = [
+    5224073836,
+]
+
+SUBGRAM_TOKEN = "82f2220177148e659f64fef91998f91143b1d6726e9667344d95f8403c65e599" # токен subgram
+FLYER_KEY = "FL-ieLinR-loBQXa-gYRkOi-MzBloc"
+
+admin_username = "@UserAdm11" #свой username
+admin_link = "https://t.me/UserAdm11"
+
+nac_1 = [3] # 1 Уровень награды за реферала
+nac_2 = [3.6] # 2 Уровень награды за реферала
+nac_3 = [8] # 3 Уровень награды за реферала
+
+refs_boost = [0] # Количество рефералов в неделю для вывода (с бустом)
+refs_noboost = [5] # Количество рефералов в неделю для вывода (без буста)
+
+task_grant = [0.5] # Награда за задание
+
+click_grant = [0.1] # Награда за клик
+
+boost_cost = [500] # Стоимость буста
+
+button_subgram = [True] 
+
+GIFT_AMOUNT = [0.5] # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 360 # Задержка на кликер (в секундах)
+
+#основной канал
+channel_osn = "https://t.me/GalaxyStarsChanel"
+#чат
+chater = "https://t.me/GalaxyStarsChat"
+id_chat = -1002275668096
+#канал выплат
+channel_viplat = "https://t.me/GalaxyStarsCashout"
+channel_viplat_id = -1002627258683
+#канал выигрышей мини-игры:
+channel_link = "https://t.me/GalaxyStarsGames"
+id_channel_game = -1002805400237
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
